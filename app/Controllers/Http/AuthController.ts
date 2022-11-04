ff004cdf85f4bf68cdd4019640ea515038732ae0
Luisa <luisasousascalioni@gmail.com>
2022-11-04 10:22:51 -0300

    foi

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -4,33 +4,25 @@ import RegisterUserValidator from 'App/Validators/RegisterUserValidator'
 
 export default class AuthController {
 
-    public async register({ request }: HttpContextContract) {
-        const data = await request.validate(RegisterUserValidator)
-        const user = await User.create(data)
-        return user
+  public async register({ request }: HttpContextContract) {
+    const data = await request.validate(RegisterUserValidator)
+    const user = await User.create(data)
+    return user
+  }
 
+  public async login({ request, auth, response }: HttpContextContract) {
+    try {
+      const { email, password } = request.all()
+      const token = await auth.use('api').attempt(email, password, {
+        expiresIn: '1day'
+      })
+      const user = await User.findByOrFail("email", email)
+      return { token, user }
+    } catch (error) {
+      response.status(401).send("Login ou senha incorretos!!!")
     }
-
-    public async login({ request, auth, response }: HttpContextContract) {
-        try {
-            const { email, password } = request.all()
-            const token = await auth.use('api').attempt(email, password, {
-                expiresIn: '1day'
-            })
-            const user = await User.findByOrFail("email", email)
-            return { token, user }
-        } catch (error) {
-            response.status(401).send("Login ou senha incorretos!!!")
-        }
-
-
-
-    }
-
+  }
 
 }
-
-
-
 
 }
