e4e7e0a662fbdf82b38d6cb109123f0a88a289e8
luisa <luisasousascalioni@gmail.com>
2022-11-11 07:56:03 -0300

    alt

--- database/migrations/1667566232944_legendas.ts
+++ database/migrations/1667566232944_legendas.ts
@@ -6,6 +6,10 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('nome_legendas')
+      table.string('nome_local')
+      table.string('nome_data')
+      
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
--- database/migrations/1667566278451_pastas.ts
+++ database/migrations/1667566278451_pastas.ts
@@ -6,6 +6,11 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('nome_pastas')
+      table.string('nome_nome')
+      table.string('nome_data')
+      table.string('nome_descrição')
+
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
