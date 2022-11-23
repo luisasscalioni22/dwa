e14e243164dce2e0bb92e8a0baa501dfd049a734
luisa <luisasousascalioni@gmail.com>
2022-11-23 17:29:33 -0300

    avanço

--- database/migrations/1667566232944_legenda.ts
+++ database/migrations/1667566232944_legenda.ts
@@ -1,12 +1,12 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'legendas'
+  protected tableName = 'legenda'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('nome_legendas')
+      table.string('nome_legenda')
       table.string('nome_local')
       table.string('nome_data')
       
